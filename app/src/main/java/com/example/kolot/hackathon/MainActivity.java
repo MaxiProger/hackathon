213259f6b8551a1376809ea5d002aa0916c9f6a3
MaxiProger <kolotseymax@gmail.com>
2017-11-25 02:46:18 +0300

    Add authorization in VK

--- app/src/main/java/com/example/kolot/hackathon/MainActivity.java
+++ app/src/main/java/com/example/kolot/hackathon/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.kolot.hackathon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.util.VKUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET}, 1);
+
+            }
+
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(String s : fingerprints){
+            stringBuilder.append(s);
+        }
+
+        VKSdk.login(this, "friends");
+        Log.d("tag", stringBuilder.toString());
     }
 }
